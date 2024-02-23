5fa081a85a9a1036dad8ac46dc1acb9269f4967b
Christof Röhl <userpattern@gmail.com>
2024-02-23 08:19:00 +0100

    solved second challenge (LOTR)

---
+++ nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+
+export default function Volumes() {
+  const router = useRouter();
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  function handleClick() {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Get random volume</button>
+    </>
+  );
+}
